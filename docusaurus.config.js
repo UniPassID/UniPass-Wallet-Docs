cc53462f3bf5f87d189f673ac4bfd2fe07169f18
大海 <mp@sea.team>
2022-07-21 17:48:09 +0800

    UniPass Wallet

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
             position: "left",
             label: "Tutorial",
           },
-          { to: "/blog", label: "Blog", position: "left" },
+          // { to: "/blog", label: "Blog", position: "left" },
           {
             href: "https://github.com/facebook/docusaurus",
             label: "GitHub",
