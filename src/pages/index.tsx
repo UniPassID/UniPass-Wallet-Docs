58dc0aca23f35265e03616e017cf9c5667e21d00
stwith <jingsongcai@126.com>
2022-08-18 19:36:24 +0800

    delete homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/introduction/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            UniPass Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
